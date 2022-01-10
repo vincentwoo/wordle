8bb16d40f09b8ae7ef03b2d7f01d3d401d5f504b
Vincent Woo <me@vincentwoo.com>
2022-01-10 03:27:19 -0800

    some simple optimizations

--- benchmark.rb
+++ benchmark.rb
@@ -1,57 +1,71 @@
 $stdout.sync = true
 
-lists = File.read('word_lists.txt').split("\n\n")
-solutions, possible = lists.map { |s| s.split("\n").map(&:chars) }
-possible += solutions
+require 'stackprof'
+StackProf.run(mode: :cpu, raw: true, out: 'perf2.dump') do
+  lists = File.read('word_lists.txt').split("\n\n")
+  solutions, possible = lists.map { |s| s.split("\n").map(&:chars) }
+  possible += solutions
 
-puzzles = File.read('benchmark.txt').split("\n")
-puzzles = puzzles.first(ARGV[0].to_i) if ARGV[0]
+  puzzles = File.read('benchmark.txt').split("\n")
+  puzzles = puzzles.first(ARGV[0].to_i) if ARGV[0]
 
-def remaining_solutions solutions, rejected, present, found
-  solutions.select { |soln|
-    (soln & rejected).length == 0 &&
-    found.all? { |c, i| soln[i] == c } &&
-    present.all? { |c, i| soln[i] != c && soln.include?(c) }
-  }
-end
-
-def check_guess guess, soln
-  rejected = []
-  present = []
-  found = []
-  guess.each.with_index do |c, i|
-    if soln[i] == c
-      found.push [c, i]
-    elsif soln.include?(c)
-      present.push [c, i]
-    else
-      rejected.push c
-    end
-  end
-  [rejected.uniq.sort, present, found]
-end
-
-total_cost = puzzles.sum do |puzzle|
-  _solutions = solutions
-  guesses = ['roate']
-  while true
-    break if guesses.last == puzzle
-
-    _solutions = remaining_solutions _solutions, *check_guess(guesses.last.chars, puzzle)
-
-    memo = {}
-    guess = possible.min_by { |guess|
-      score = _solutions.sum do |solution|
-        mask = check_guess(guess, solution)
-        memo[mask] ||= remaining_solutions(_solutions, *mask).count
+  def remaining_solutions solutions, guess, mask
+    i = 4
+    while i >= 0
+      m = mask & 3
+      solutions = case m
+      when 0
+        solutions.reject { |soln| soln.include? guess[i] }
+      when 1
+        solutions.select { |soln| soln.include?(guess[i]) && soln[i] != guess[i] }
+      when 2
+        solutions.select { |soln| soln[i] == guess[i] }
       end
-      score -= 1 if _solutions.include?(guess)
-      score
-    }.join
-    guesses.push guess
+      mask >>= 2
+      i -= 1
+    end
+    solutions
   end
-  puts "Solved #{puzzle.upcase} in #{guesses.length}: #{guesses.map(&:upcase).join(', ')}"
-  guesses.length
-end
 
-puts "Solved all #{puzzles.length} puzzles in #{total_cost} guesses, avg #{total_cost.to_f / puzzles.length} per"
+  def check_guess guess, soln
+    i = 0
+    ret = 0
+    while i < 5
+      ret <<= 2
+      if soln[i] == guess[i]
+        ret += 2
+      elsif soln.include?(guess[i])
+        ret += 1
+      end
+      i += 1
+    end
+    ret
+  end
+
+  total_cost = puzzles.sum do |puzzle|
+    _solutions = solutions
+    guesses = ['roate']
+    while true
+      break if guesses.last == puzzle
+      guess = guesses.last.chars
+
+      mask = check_guess(guess, puzzle)
+      _solutions = remaining_solutions _solutions, guess, mask
+
+      guess = possible.min_by { |guess|
+        memo = {}
+        score = _solutions.sum do |solution|
+          mask = check_guess(guess, solution)
+          memo[mask] ||= remaining_solutions(_solutions, guess, mask).count
+        end
+        score -= 1 if _solutions.include?(guess)
+        score
+      }.join
+      guesses.push guess
+    end
+    puts "Solved #{puzzle.upcase} in #{guesses.length}: #{guesses.map(&:upcase).join(', ')}"
+    guesses.length
+  end
+
+  puts "Solved all #{puzzles.length} puzzles in #{total_cost} guesses, avg #{total_cost.to_f / puzzles.length} per"
+end
